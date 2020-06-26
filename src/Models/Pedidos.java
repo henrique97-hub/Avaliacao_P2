31d9035e61a0751ad88e8714da9f4a50a6e99c4a
henrique97-hub <61150198+henrique97-hub@users.noreply.github.com>
2020-06-26 16:18:13 -0300

    Implementaçao em pedidos e usuarios
    Foram criados todos os metodos

--- src/Models/Pedidos.java
+++ src/Models/Pedidos.java
@@ -6,35 +6,43 @@ public class Pedidos {
         protected String id;
         protected String descricao;
         protected double valor;
+        protected String estado;
+        protected String pegamentos;
 
 // metodos especiais
 
     public String getId() {
-        return id;
-    }
+        return id;}
 
     public void setId(String id) {
-        this.id = id;
-    }
+        this.id = id;}
 
     public String getDescricao() {
-        return descricao;
-    }
+        return descricao; }
 
     public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+        this.descricao = descricao; }
 
     public double getValor() {
-        return valor;
-    }
+        return valor;}
 
     public void setValor(double valor) {
-        this.valor = valor;
-    }
+        this.valor = valor; }
+
+    public String getEstado() {
+        return estado; }
+
+    public void setEstado(String estado) {
+        this.estado = estado; }
+
+    public String getPegamentos() {
+        return pegamentos; }
+
+    public void setPegamentos(String pegamentos) {
+        this.pegamentos = pegamentos; }
 
 
-// metodos publicos e privados
+    // metodos publicos e privados
 
     private String geradorId(){
     Random random = new Random();
@@ -43,11 +51,46 @@ public class Pedidos {
         idGerado += random.nextInt(10);
     return idGerado;}
 
-
-    public void criarPedidos(String descricao, double valor){
-
-
+    // verificar senha
+    public void criarPedidos(String descricao, double valor, String estado, String pagamentos){
+        this.descricao = descricao;
+        this.valor = valor;
+        if (estado == "preparacao"){
+            System.out.println("O pedido esta: " +Estado.PREPARACAO);
+        } else {
+            System.out.println("erro de estado");
+        }
+        System.out.println("O pedido foi criado com sucesso!");
+        System.out.println("O id do pedido é: "+this.geradorId());
+        System.out.println("O pedido é: "+this.getDescricao());
+        System.out.println("O valor do pedido é: "+this.getValor());
+        System.out.println("O estado do pedido é:" +this.estado);
+        if (pagamentos == "dinheiro"){
+            System.out.println("A forma de pagamento é: "+Pagamentos.DINHEIRO);
+        } else if (pagamentos == "debito"){
+            System.out.println("A forma de pagamento é: "+Pagamentos.DEBITO);
+        } else if (pagamentos =="credito"){
+            System.out.println("A forma de pagamento é :"+Pagamentos.CREDITO);}
+        else if (pagamentos == "va"){
+        System.out.println("A forma de pagamento é:" +Pagamentos.VALE_ALIMENTACAO);
+    }  else if (pagamentos == "vr"){
+            System.out.println("A forma de pagamento é:" +Pagamentos.VALE_REFEICAO);}
+        else {
+            System.out.println("Forma de pagamento não aceita");
+        }
     }
 
+    public void alterarPedidos(String estado){
+       // verificar senha
+        this.setEstado(this.getEstado());
+    }
+
+    public void verificarPedidos(){
+        System.out.println("Informaçoes gerais do pedido:");
+        System.out.println("o id do pedido é: " +this.geradorId());
+        System.out.println("O valor do pedido é: "+this.getValor());
+        System.out.println("A forma de pagamento é: "+this.getPegamentos());
+        System.out.println("O estado do pedido é: "+this.getEstado());
+    }
 
 }
--- src/Models/Usuario.java
+++ src/Models/Usuario.java
@@ -1,4 +1,39 @@
 package Models;
 
-public class Usuario {
+public class Usuario extends Pedidos implements Autentica {
+// atributos
+    private String name = "pizzaria";
+    private String email = "pizzaria@pizzaria";
+    private int senha = 123456;
+
+// implementa o metodo abstrato da interface do login
+    @Override
+    public void login(String nome, String email, int senha) {
+
+    }
+// metodos especiais
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
 }
